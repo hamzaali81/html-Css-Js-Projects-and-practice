1e95b500e07563e83c22276e556b819e80f837ad
hamzaali <hamza54834@gmail.com>
2020-04-13 12:16:15 -0700

    Asynchronous asnc/await ajax

--- js/Asynchronous js/script.js
+++ js/Asynchronous js/script.js
@@ -99,69 +99,88 @@
 
 
 //Promise consume
-const getIDs = new Promise((resolve, reject) => {
-    setTimeout(() => {
-        // reject([101, 102, 103, 104])
-        resolve([101, 102, 103, 104])
-    }, 1500)
-})
+// const getIDs = new Promise((resolve, reject) => {
+//     setTimeout(() => {
+//         // reject([101, 102, 103, 104])
+//         resolve([101, 102, 103, 104])
+//     }, 1500)
+// })
 
-const getReceipe = recID => {
-    return new Promise((resolve, reject) => {
-        setTimeout(ID => {
-            const receipe = {
-                title: 'chips',
-                author: 'hamza'
+// const getReceipe = recID => {
+//     return new Promise((resolve, reject) => {
+//         setTimeout(ID => {
+//             const receipe = {
+//                 title: 'chips',
+//                 author: 'hamza'
 
-            }
-            resolve(`${ID}: ${receipe.title}`);
+//             }
+//             resolve(`${ID}: ${receipe.title}`);
 
-        }, 1500, recID);
-    });
-};
+//         }, 1500, recID);
+//     });
+// };
 
-const getRelated = publisher => {
-        return new Promise((resolve, reject) => {
-            setTimeout(pub => {
-                const receipe = {
-                    title: 'burger',
-                    publisher: 'ahmed'
-                }
-                resolve(`${pub} : ${receipe.title}`)
-            }, 1500, publisher)
+// const getRelated = publisher => {
+//         return new Promise((resolve, reject) => {
+//             setTimeout(pub => {
+//                 const receipe = {
+//                     title: 'burger',
+//                     publisher: 'ahmed'
+//                 }
+//                 resolve(`${pub} : ${receipe.title}`)
+//             }, 1500, publisher)
+//         })
+//     }
+// getIDs                           //this part as async shows
+//     .then(IDs => {
+//         console.log(IDs);
+//         return getReceipe(IDs[2]);
+//         // return getRelated(IDs[3]);
+//     })
+//     .then(receipe => {
+//         console.log(receipe);
+//         // return getRelated(receipe.publisher);
+//         return getRelated('hamza');
+//     })
+//     .then(receipe => {
+//         console.log(receipe);
+//     })
+//     .catch(error => {
+//         // console.log(error);
+//         console.log('error!!');
+//     });
+
+// async function getReceipesAw() {
+//     const IDs = await getIDs;
+//     console.log(IDs);
+//     const receipe = await getReceipe(IDs[3]);
+//     console.log(receipe);
+//     const related = await getRelated('hamza');
+//     console.log(related);
+//     return receipe;
+// }
+// // const rec = getReceipesAw();
+// // console.log(rec);
+
+// getReceipesAw().then(result => {
+//     console.log(`${receipe}: result is the best ever!`);
+// });
+
+
+
+//Making Ajax Calls with fetch and promises
+function getWeather(woeid) {
+    fetch(`https://crossorigin.me/https://www.metaweather.com/api/location/${woeid}/`)
+        .then(result => {
+            console.log(result);
+            return result.json();
         })
-    }
-    // getIDs                           //this part as async shows
-    //     .then(IDs => {
-    //         console.log(IDs);
-    //         return getReceipe(IDs[2]);
-    //         // return getRelated(IDs[3]);
-    //     })
-    //     .then(receipe => {
-    //         console.log(receipe);
-    //         // return getRelated(receipe.publisher);
-    //         return getRelated('hamza');
-    //     })
-    //     .then(receipe => {
-    //         console.log(receipe);
-    //     })
-    //     .catch(error => {
-    //         // console.log(error);
-    //         console.log('error!!');
-    //     });
+        .then(data => {
+            // console.log(data);
+            console.log('Temperature in ${data.title} stay between ${today.min_temp} and ${today.max_temp}')
+        })
+        .catch(error =>
+            console.log(error));
 
-async function getReceipesAw() {
-    const IDs = await getIDs;
-    console.log(IDs);
-    const receipe = await getReceipe(IDs[3]);
-    console.log(receipe);
-    const related = await getRelated('hamza');
-    console.log(related);
-    return receipe;
 }
-// const rec = getReceipesAw();
-// console.log(rec);
-
-getReceipesAw().then(result => {
-    console.log(`${receipe}: result is the best ever!`);
-});
+getWeather(2487956);
