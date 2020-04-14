587cf08cde57fce1e5bba93c4ade6d326fca8377
hamzaali <hamza54834@gmail.com>
2020-04-14 04:53:39 -0700

    lexical arrow function

--- js/ES6/script.js
+++ js/ES6/script.js
@@ -33,27 +33,96 @@
 
 
 //Arrow function
-const years = [1990, 1999, 1992, 1996, 1997, 1998];
+// const years = [1990, 1999, 1992, 1996, 1997, 1998];
 
+// //ES5
+// var ages5 = years.map(function(el) {
+//     return 2020 - el;
+// })
+// console.log(ages5);
+
+
+
+// //ES6
+// let ages6 = years.map(el => 2020 - el);
+// console.log(ages6);
+
+// ages6 = years.map((el, index) => `Age element ${index + 1}: ${2020 - el} .`)
+// console.log(ages6)
+
+// ages6 = years.map((el, index) => {
+//     const now = new Date().getFullYear();
+//     const age = now - el;
+//     return `Age element ${index+1} ${age}`
+// })
+
+// console.log(ages6)
+
+//Lecture: Arrow function 2
 //ES5
-var ages5 = years.map(function(el) {
-    return 2020 - el;
-})
-console.log(ages5);
-
+// var box5 = {
+//     color: 'green',
+//     position: 1,
+//     clickMe: function() {
+//         var self = this;
+//         document.querySelector('.green').addEventListener('click', function() {
+//             var str = 'This is box number' + self.position + 'and it is' + self.color;
+//             alert(str);
+//         })
+//     }
+// }
+// box5.clickMe();
 
 
 //ES6
-let ages6 = years.map(el => 2020 - el);
-console.log(ages6);
+// var box6 = {
+//         color: 'green',
+//         position: 1,
+//         clickMe: function() {
+//             // var self = this;
+//             document.querySelector('.green').addEventListener('click', () => {
+//                 var str = 'This is box number' + this.position + 'and it is' + this.color;
+//                 alert(str);
+//             })
+//         }
+//     }
+//box6.clickMe();
 
-ages6 = years.map((el, index) => `Age element ${index + 1}: ${2020 - el} .`)
-console.log(ages6)
 
-ages6 = years.map((el, index) => {
-    const now = new Date().getFullYear();
-    const age = now - el;
-    return `Age element ${index+1} ${age}`
-})
+// var box7 = {
+//     color: 'green',
+//     position: 1,
+//     clickMe: () => {
+//         var self = this;
+//         document.querySelector('.green').addEventListener('click', function() {
+//             var str = 'This is box number' + self.position + 'and it is' + self.color;
+//             alert(str);
+//         })
+//     }
+// }
+// box7.clickMe();
 
-console.log(ages6)
+
+
+function Person(name) {
+    this.name = name;
+}
+Person.prototype.myFriend5 = function(friends) {
+
+    var arr = friends.map(function(el) {
+        return this.name + ' is friends with ' + el;
+    }.bind(this))
+    console.log(arr);
+}
+
+var friends = ['hamza', 'hassan', ';ahmed'];
+new Person('john').myFriend5(friends);
+//ES6
+Person.prototype.myFriend6 = function(friends) {
+
+    var arr = friends.map(el => `${this.name} is friends with ${el}`);
+    console.log(arr);
+}
+
+new
+Person('ali').myFriend6(friends);
