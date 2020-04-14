7cfcdf956ffde5f1e756d2023d2cd30982b11fc8
hamzaali <hamza54834@gmail.com>
2020-04-14 03:23:17 -0700

    js ES6 arrow function

--- js/ES6/script.js
+++ js/ES6/script.js
@@ -30,3 +30,30 @@
 // // var lastname = 'ali';
 // // var n = firstname + lastname;
 // // console.log(n.startsWith('h'));
+
+
+//Arrow function
+const years = [1990, 1999, 1992, 1996, 1997, 1998];
+
+//ES5
+var ages5 = years.map(function(el) {
+    return 2020 - el;
+})
+console.log(ages5);
+
+
+
+//ES6
+let ages6 = years.map(el => 2020 - el);
+console.log(ages6);
+
+ages6 = years.map((el, index) => `Age element ${index + 1}: ${2020 - el} .`)
+console.log(ages6)
+
+ages6 = years.map((el, index) => {
+    const now = new Date().getFullYear();
+    const age = now - el;
+    return `Age element ${index+1} ${age}`
+})
+
+console.log(ages6)
