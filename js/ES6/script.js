4860e7c1326c5ef942b202cdcfe39393b0ef1a45
hamzaali <hamza54834@gmail.com>
2020-04-14 02:35:52 -0700

    js ES6

---
+++ js/ES6/script.js
@@ -0,0 +1,32 @@
+////////////////////////////
+//Blocks and IIFE
+//ES5
+// (function() {
+//     var c = 3;
+// })();
+// // console.log(c);
+// //ES6
+// {
+//     const a = 1;
+//     let b = 2;
+//     var d = 5;
+// }
+// // console.log(a + b);
+// console.log(d);
+
+
+// //STRING IN ES6
+// //ES6
+// const firstname = 'hamza';
+// const lastname = 'ali';
+// const n = `${firstname} ${lastname}`;
+// // console.log(n.startsWith('h'));
+// // console.log(n.endsWith('h'));
+// console.log(n.includes(' '));
+// console.log(`${firstname} `.repeat(10));
+
+// //ES5
+// // var firstname = 'hamza';
+// // var lastname = 'ali';
+// // var n = firstname + lastname;
+// // console.log(n.startsWith('h'));
