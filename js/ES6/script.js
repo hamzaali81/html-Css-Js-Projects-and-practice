721845cbb4d430add12d23cb893e5c67a244ba67
hamzaali <hamza54834@gmail.com>
2020-04-15 07:51:44 -0700

    es6 new features study

--- js/ES6/script.js
+++ js/ES6/script.js
@@ -1,4 +1,18 @@
 ////////////////////////////
+
+//Scope
+// for (let i = 0; i <= 5; i++) {
+//     setTimeout(() => {
+//         console.log(i);
+//     }, 1500)
+// }
+// for (var i = 0; i <= 5; i++) {
+//     // setTimeout(() => {
+//     //     console.log(i);
+//     // }, 1500)
+//     console.log(i);
+// }
+
 //Blocks and IIFE
 //ES5
 // (function() {
@@ -152,10 +166,146 @@
 
 
 
-function calAge(year) {
-    const age = new Date().getFullYear() - year;
-    return [age, 65 - age]
+// function calAge(year) {
+//     const age = new Date().getFullYear() - year;
+//     return [age, 65 - age]
+// }
+// const [age, retirement] = calAge(1999);
+// console.log(age);
+// console.log(retirement);
+
+
+//Spread operator
+
+// function addFourages(a, b, c, d) {
+//     return a + b + c + d;
+// }
+// var sum1 = addFourages(18, 30, 12, 21);
+// console.log(sum1);
+
+
+// //ES5
+// var ages = [18, 30, 12, 21];
+// var sum2 = addFourages.apply(null, ages);
+// console.log(sum2);
+
+// //ES6
+// const sum3 = addFourages(...ages);
+// console.log(sum3)
+
+// const family1 = ['hamza', 'ali', 'ahmed'];
+// const family2 = ['hassan', 'faizan', 'azhar'];
+// const bigfamily = [...family1, 'fahad', ...family2];
+// console.log(bigfamily);
+
+// const h = document.querySelector('h1');
+// const boxes = querySelectorAll('.box');
+// const all = [h, ...boxes];
+
+// Array.from(all).forEach(cur => cur.style.color = 'purple');
+
+
+//Map
+//new key value data structure data structure
+
+
+// const question = new Map();
+// question.set('question', 'what is the official name of the latest major javascript version?')
+// question.set(1, 'ES5');
+// question.set(2, 'ES6');
+// question.set(3, 'ES2015');
+// question.set(4, 'ES7');
+// question.set('correct', 3);
+// question.set(true, 'Correct answer :D');
+// question.set(false, 'Wrong please try again!');
+
+// console.log(question.get('question'));
+// console.log(question.size);
+// if (question.has(4)) {
+//     // question.delete(4);
+//     console.log('Answe 4 is here')
+// }
+// //question.clear;
+// question.forEach((value, key) => {
+//     console.log(`This is ${key}, and it's set to ${value}`);
+// })
+
+// for (let [key, value] of question.entries()) {
+//     // console.log(`This is ${key}, and it's set to ${value}`)
+//     if (typeof(key) === 'number') {
+//         console.log(`Answer ${key}: ${value}`)
+//     }
+// }
+
+// const ans = parseInt(prompt('Write the correct answer '))
+
+// console.log(question.get(ans === question.get('correct')))
+
+
+//Class Inheritance
+//ES5
+// function Player(name, age) {
+//     this.name = name;
+//     this.age = age;
+// }
+// var team1 = new Player('hamza', 21)
+// var team2 = new Player('ali', 22)
+// console.log(team1)
+
+// Player.prototype.lastname = 'hassan';
+//Player.prototype.greet=function(){
+//console.log("hello this is "+this.name+"and age is"+this.age)
+//}
+// console.log(team1.lastname)
+
+//ES6
+// class Player {
+//     constructor(name, age) {
+//         this.name = name;
+//         this.age = age;
+//     }
+//     greet() {
+//         console.log("hello this is " + this.name + "and age is" + this.age)
+
+//     }
+// }
+
+// const team1 = new Player('hamza', 21); //Specific class
+// const pak = new Team('babar', 26); //generic class
+// console.log(team1);
+
+//New Concept inheritance
+
+class People {
+    constructor(name, job, country) {
+        this.name = name;
+        this.job = job;
+        this.country = country;
+    }
+    sayHi() {
+        console.log(`Hi ,I'm ${name}, I'm ${job} work in ${country}`)
+    }
 }
-const [age, retirement] = calAge(1999);
-console.log(age);
-console.log(retirement);
+class Player extends People {
+    constructor(name, age, job, country) {
+        super(name, job, country); //super initialize people cinstructor
+        // this.name = name;
+        this.age = age;
+    }
+    greet() {
+        console.log("hello this is " + this.name + "and age is" + this.age)
+
+    }
+    get nick() {
+        return this.nick.toUpperCase();
+    }
+    set nick(value) {
+        this.nickname = value;
+    }
+    static description() { //not work instance work direct class
+        console.log("This is description")
+    }
+}
+
+const team1 = new Player('hamza', 21, 'cricketer', 'pakistan');
+const team2 = new People('hamza', 'student', 'pakistan')
