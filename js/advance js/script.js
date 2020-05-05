84f187d99810cc6aa0368d73c0ce82956211cd4a
hamzaali <hamza54834@gmail.com>
2020-05-05 05:28:14 -0700

    advance js classes

--- js/advance js/script.js
+++ js/advance js/script.js
@@ -19,6 +19,57 @@
 
 
 //ES6 Constructor
+// class Person6{
+//     constructor(name,yearofbirth,job){
+//         this.name=name;
+//              this.yearofbirth=yearofbirth;
+//              this.job=job;
+//     }
+//     calAge(){
+//         var age= 2020-yearofbirth;
+//         console.log(age)
+//     }
+//     static greetings(){
+//         console.log("hey there")
+//     }
+// }
+
+// var john2=new Person6('hamza',1990,'designer');
+// Person6.greetings()
+
+// john2.calAge();
+
+//Sub classes
+var Person5=function(name,yearofbirth,job){
+this.name= name;
+this.yearofbirth= yearofbirth;
+this.job=job;
+}
+
+Person5.prototype.calculateAge=function(){
+    var age=new Date().getFullYear()-this.yearofbirth;
+    console.log(age)
+}
+var Athelet5=function(name,yearofbirth,job,olympicGames,medals){
+Person5.call(this,name,yearofbirth,job);
+this.olympicGames=olympicGames;
+this.medals=medals;
+}
+
+Athelet5.prototype=Object.create(Person5.prototype);
+
+Athelet5.prototype.wonMedal=function(){
+    this.medals++;
+    console.log(this.medals)
+}
+
+var johnAthelete=new Athelet5('john',1999,'swimmwer',3,10);
+johnAthelete.calculateAge();
+johnAthelete.wonMedal()
+
+
+//ES6
+//ES6 Constructor
 class Person6{
     constructor(name,yearofbirth,job){
         this.name=name;
@@ -26,16 +77,39 @@ class Person6{
              this.job=job;
     }
     calAge(){
-        var age= 2020-yearofbirth;
+        var age= new Date().getFullYear() - this.yearofbirth;
         console.log(age)
     }
-    static greetings(){
-        console.log("hey there")
-    }
+    
 }
 
-var john2=new Person6('hamza',1990,'designer');
-Person6.greetings()
+class Athelet6 extends Person6{
+    constructor(name,yearofbirth,job,olympicGames,medals){
+ super(name,yearofbirth,job);
+ this.olympicGames=olympicGames;
+ this.medals=medals;
+}
+wonMedal(){
+    this.medals++;
+    console.log(this.medals)
+}
+}
 
-// john2.calAge();
+var johnAthelete2=new Athelet6('john',1999,'swimmwer',3,10);
+console.log(johnAthelete2)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
