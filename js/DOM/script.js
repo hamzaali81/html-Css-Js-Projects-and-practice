ab5e89efba6c14d692778ec5209d0b9df6ceeb7b
hamzaali <hamza54834@gmail.com>
2020-05-09 10:36:19 -0700

    DOM practice

--- js/DOM/script.js
+++ js/DOM/script.js
@@ -1,2 +1,5 @@
-var a=document.getElementById('first');
-a=''
+var a=document.querySelector('#first');
+
+var b=Math.floor(Math.random()*6) + 1;
+console.log(b)
+var a=document.querySelector('#first').innerHTML='<b>'+b+'</b';
