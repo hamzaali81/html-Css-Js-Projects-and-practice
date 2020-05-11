5fc0f50b04d18d55ab3cfcac01aff818a65611f5
hamzaali <hamza54834@gmail.com>
2020-05-11 14:57:12 -0700

    js

---
+++ js/js practice/script.js
@@ -0,0 +1,63 @@
+//prompt always in string value if
+//+prompt
+
+//1.Set Attribute
+//(first Set Value,Implement);
+//e.g('style','color')
+// function save(){
+//     console.log('Hello world')
+// }
+// document.getElementById('btn').setAttribute('onclick','save()');
+// // console.log(a)
+
+//2.appendChild
+
+// var elementCreate=document.createElement('h2');
+// var add = document.createTextNode('ES6');
+
+
+// elementCreate.appendChild(add);
+
+// document.getElementById('con').appendChild(elementCreate);
+
+//querySelector
+// var a='hello';
+// document.querySelector('#con').innerHTML='<h1>'+a+'</h2>';
+
+//3.Css Styling
+//1.className
+//2.classList
+//3.style
+
+
+//4.childNode
+
+var a=document.getElementById('con').childNodes[1];
+console.log(a)
+
+var x=5;
+var y=x++ + ++x + x + --x + x-- - x;
+    //   5   +   7    7   6      6     5
+console.log(y);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
