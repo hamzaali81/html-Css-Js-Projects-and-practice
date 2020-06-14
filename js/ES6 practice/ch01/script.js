26b97de75d26a9ca9970fe32958e4041aa5b1c78
hamzaali <hamza54834@gmail.com>
2020-06-13 20:03:21 -0700

    map and foreach

---
+++ js/ES6 practice/ch01/script.js
@@ -0,0 +1,117 @@
+//Array helper ES6
+// var arr=["hamza","ali"];
+// arr.forEach(el=>console.log(el));
+
+
+// //Without Array Helper ES5
+// var list=["karachi","lahore"];
+// for(var i=0;i<list.length;i++){
+//     console.log(list[i])
+// }
+
+// Example forEach
+
+// var  lists=[99,18,70,80,17]
+// var per=100;
+
+// lists.forEach(el=>{
+//     var result=el/per;
+//     console.log(result);
+    
+// })
+
+// var a=100;
+
+// var b=[60,80,78];
+// for (i=0;i<b.length;i++){
+//     var c=b[i]/a;
+//     console.log("result 9 class "+c);
+    
+// }
+
+
+//Map Method
+// var number=[1,2,3];
+// var list=[];
+
+// // for (var i=0; i<number.length;i++){
+// //     var res=list.push(number[i]*2);
+// //     console.log(res);
+    
+// // }
+
+// var a=number.map(el=>{
+// var b=el*2;
+// console.log(b)
+// })
+
+// console.log(a);
+
+var cars=[
+    {model:'buick',rate:'cheap'},
+    {model:'camaro',rate:'expensive'},
+]
+cars.map(el=>{
+    var models=el.model;
+    console.log(models)
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
