b764ec73151348502a6cb2604403b8592f9fb897
hamzaali <hamza54834@gmail.com>
2020-06-14 19:23:18 -0700

    find helper complete

--- js/ES6 practice/ch01/script.js
+++ js/ES6 practice/ch01/script.js
@@ -47,14 +47,164 @@
 
 // console.log(a);
 
-var cars=[
-    {model:'buick',rate:'cheap'},
-    {model:'camaro',rate:'expensive'},
+// var cars=[
+//     {model:'buick',rate:'cheap'},
+//     {model:'camaro',rate:'expensive'},
+// ]
+// cars.map(el=>{
+//     var models=el.model;
+//     console.log(models)
+// })
+
+
+
+//Filter
+
+// var items=[
+//     {name:'banana',type:'fruit',quantity:4,price:30},
+//     {name:'orange','type':'fruit',quantity:10,price:50},
+//     {name:'cucumber',type:'vegetable',quantity:7,price:40}
+// ]
+
+
+// // var list=[];
+
+// // // for(var i=0;i<items.length;i++){
+// // //     if(items[i].type==='fruit'){
+// // //       var a=  list.push(items[i]);
+// // //         console.log(a);
+// // //     }
+// // // }
+
+
+// // items.filter(el=>{
+// //     if(el.type==='fruit'){
+        
+// //         //  console.log(el);
+// // return true;
+// //     }
+// // })
+
+// items.filter(el=>{
+//     if(el.type==='fruit' && el.quantity > 8 && el.price > 41 ){
+//            console.log(el);
+                  
+//     }
+// })
+
+//find Helper
+// var  users=[
+//     {name:'hamza'},
+//     {name:'ahmed'},
+//     {name:'ali'}
+// ]
+// var user;
+// for(var i=0; i<users.length;i++){
+//     if(users[i].name=='hamza'){
+//         user=users[i];
+//         console.log(user);
+        
+//     }
+// }
+
+// users.find(el=>{
+// if(el.name==='hamza'){
+
+//     console.log(el)
+// }
+// })
+
+// function Car(model){
+// this.model=model;
+// }
+// var cars=[
+//     new Car('ferrari'),
+//     new Car('bucati')
+// ]
+// cars.find(el=>{
+//     var a=el.model;
+//     console.log(a);
+    
+    
+// })
+
+var posts=[
+    {id:1,comment:'nice pic'},
+    {id:2,comment:'Amazing'}
 ]
-cars.map(el=>{
-    var models=el.model;
-    console.log(models)
-})
+
+var comments={postId:1,title:'old post'}
+
+function postComment(posts,comment){
+    return posts.find(function(post){
+        return post.id === comments.postId;
+       
+        
+    })
+
+}
+var a=postComment(posts,comments)
+console.log(a);
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
