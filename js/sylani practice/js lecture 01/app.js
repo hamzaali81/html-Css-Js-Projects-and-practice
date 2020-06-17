47941d2d06c85566d2d816f05df4623b7dec66e1
hamzaali <hamza54834@gmail.com>
2020-06-17 00:08:01 -0700

    lecture 03

---
+++ js/sylani practice/js lecture 01/app.js
@@ -0,0 +1,11 @@
+// var a=10;
+// var b="a";
+// console.log(a*b);
+
+
+
+var a=10;
+var c= a++ + ++a + a++ + a++ + a++;
+    //    10     13   13    14    16
+       console.log(c);
+       
---
+++ js/sylani practice/lect02/app.js
@@ -0,0 +1,34 @@
+// var a=+prompt("Enter the value", "your value is")
+
+// // var b=a.parseInt+10;
+// var b=parseInt("a") + 10;
+
+// console.log("output is"+b);
+
+
+//Push & pop 
+//pop delete last index
+//push add last index
+
+
+//shift & unshift
+//shift delete first index
+//unshift add index
+
+
+//splice & slice
+
+
+//splice(index,how many(1,2...),add element)
+// var a=['hamza','ali','ahmed'];
+// a.splice(0,2,'kaka')
+// console.log(a);
+
+
+//slice copy
+//(index(start),copy(how many))
+var students=['kaka','messi','ronaldo'];
+var a=students.slice(0,2);
+console.log(students);
+
+console.log(a);
