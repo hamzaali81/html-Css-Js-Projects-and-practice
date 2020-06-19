594ec552ded25625d4e21660998075343e4a5476
hamzaali <hamza54834@gmail.com>
2020-06-19 13:33:28 -0700

    7 complete 21-25

---
+++ js/Sylani Assignment2/Assignment21/app.js
@@ -0,0 +1,188 @@
+
+//1.Write a program that takes two user inputs for first and
+//last name using prompt and merge them in a new variable
+//titled fullName. Greet the user using his full name.
+
+// var firstname=prompt("Enter first Name");
+// var lastname=prompt("Enter last Name");
+
+// document.write(firstname+" "+lastname)
+
+
+// 2.Write a program to take a user input about his favorite
+// mobile phone model. Find and display the length of user
+// input in your browser
+// var favoritePhone=prompt("Enter the Fravorite mobile Phone");
+// document.write("My Fravorite Phone is: "+favoritePhone +" Length of string "+favoritePhone.length);
+
+
+
+//3. Write a program to find the index of letter “n” in the word
+// “Pakistani” and display the result in your browser .
+
+
+// var letter="Pakistan"
+
+// var a=letter.indexOf("n");
+// document.write("String of Pakistani and Index of'n':"+a);
+
+// Write a program to find the last index of letter “l” in the
+// word “Hello World” and display the result in your browser.
+
+// var letter="Hello World"
+
+// var a=letter.lastIndexOf("l");
+// document.write("Hello World last index of "+a);
+
+
+//5. Write a program to find the character at 3rd index in the
+// word “Pakistani” and display the result in your browser.
+
+// var word="Pakistan";
+
+// var a=word.charAt(3);
+// document.write("String: "+word+" Character at index 3 " + a);
+
+//6. Repeat Q1 using string concat() method.
+// var firstname=prompt("Enter the first name");
+// var lastname=prompt("Enter the last name");
+
+// var res=firstname.concat(" "+lastname);
+// document.write("Your name is "+res)
+
+// 7.Write a program to replace the “Hyder” to “Islam” in the
+// word “Hyderabad” and display the result in your browser.
+// var city="Hyderabad";
+// var res=city.replace("Hydera","Islama");
+// console.log(res);
+
+
+// 8.Write a program to replace all occurrences of “and” in the
+// string with “&” and display the result in your browser.
+// var message = “Ali and Sami are best friends. They play cricket and
+// football together.”;
+
+
+var message="Ali and Sami are best friends";
+var a=message.replaceAll("")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
