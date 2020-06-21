37abbbfea27438f398cc30152f48e2eba2da1da7
hamzaali <hamza54834@gmail.com>
2020-06-21 11:58:25 -0700

    2 portion

---
+++ js/Sylani Assignment2/Assignment22/app.js
@@ -0,0 +1,89 @@
+// var number= 2.45214;
+// var a=Math.floor(number);
+// var b=Math.ceil(number);
+// var c=Math.round(number)
+
+
+// var dice=6;
+// var a =Math.floor(Math.random()  *((6-1)+1)+1);
+// console.log(a);
+
+
+// var coin= 2;
+
+// var toss= Math.floor(Math.random()*((2 -1)+1)+1);
+
+// console.log(toss);
+
+// if(toss=== 1){
+//     console.log("head");
+    
+// }
+// function tossing(head,tail){
+//     if(toss=== head){
+//             console.log("head");
+            
+//         }
+//         else if(toss===tail){
+//             console.log('tail');
+            
+//         }
+// }
+
+// tossing(1,2);
+// console.log(a);
+
+
+
+
+
+
+
+// var dice=10;
+// var a =Math.ceil(Math.random() * 10);
+// console.log(a);
+
+// var b=prompt("Enter the hidden number 1 to 10")
+// // console.log(a);
+// if(a==b){
+//     console.log("you win");
+    
+// }
+// else{
+//     console.log("You lose");
+    
+// }
+
+
+
+// var a= 1-5;
+// Math.abs(a)
+
+
+// for (var i=1;i<=100;i++){
+//    var a=Math.floor(Math.random()* ((100 - 1)+1)+1 );
+//     console.log(a);
+    
+// }
+var a=prompt("Enter the Weight")
+
+var b=parseInt(a);
+console.log(b+"kilograms");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
