dba00ea7a04b8928bd247f4c42cd6e4d587f994f
hamzaali <hamza54834@gmail.com>
2020-06-21 13:09:01 -0700

    3 portion half

---
+++ js/Sylani Assignment2/Assignment23/app.js
@@ -0,0 +1,71 @@
+// var a=new Date();
+// console.log(a);
+
+// const monthNames = ["January", "February", "March", "April", "May", "June",
+//   "July", "August", "September", "October", "November", "December"
+// ];
+
+// var a=new Date();
+// console.log(monthNames[a.getMonth()]);
+
+
+// const day=["Monday","tuesday","wednesday","Thursday","friday","Saturday","Sunday"]
+
+// var a=new Date();
+// console.log(day[a.getDay()]);
+
+// const day=["Monday","tuesday","wednesday","Thursday","friday","Saturday","Sunday"]
+
+// var a=new Date();
+// console.log(day[a.getDay()]);
+
+// if(day[a.getDay()]===day[5]){
+// console.log("Fun night");
+
+
+// }
+// if(day[a.getDay()]===day[6]){
+
+// console.log("fun Day!");
+
+// }
+// else{
+//     console.log("Working day");
+    
+// }
+
+// var a=new Date();
+// console.log(a);
+
+// if(a<16){
+//     console.log("First fifteen day of the month");
+    
+// }
+
+// else{
+//     console.log("Last fifteen days of the month");
+    
+// }
+
+// var a=new Date().getTime();
+// console.log(a);
+
+var a=new Date();
+console.log(a);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
