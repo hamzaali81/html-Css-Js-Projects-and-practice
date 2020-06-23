03bc0e937dbd51f6113413c831b6567235314be1
hamzaali <hamza54834@gmail.com>
2020-06-23 11:44:43 -0700

    1 20 practice

--- js/sylani practice/lect15/app.js
+++ js/sylani practice/lect15/app.js
@@ -46,16 +46,89 @@
 // var a=prompt("Your age is",a);
 // console.log(a);
 
-var age=18;
-var name="hamza"
-if(age>=18 && name!=="hamza"){
-    console.log("you are egligible");
+// var name="hamza"
+// if((name==="hamza" && name==="ali")  || name==="hamza"){
+//     console.log("you are egligible");
     
-}
+// }
+
+// var arr=["hamza","ahmed","ali"];
+
+// arr.pop();
+// console.log(arr);
+// var arr=["hamza","ahmed","ali"];
+
+// arr.shift();
+// console.log(arr);
 
 
 
 
+//  var arr=["hamza","ahmed","ali","faizan","aqib"];
+// console.log(arr);
+
+//  arr.splice(2,2,"karachi"); //(index point, deleting point)
+//  console.log(arr);
+
+// var arr=["hamza","ahmed","ali","faizan","aqib"];
+// console.log(arr);
+
+//  var a=arr.slice(0,2);   //index start(numbering copy)
+//  console.log(a);
+
+// console.log(arr);
+
+// var arr=["hamza","ahmed","ali","faizan","aqib"];
+// arr.splice(1,3,"kk")
+// console.log(arr);
+
+
+
+//test
+// var a=["car1","car2","car3"]
+// var b=a.slice(0,2);
+// console.log(b);
+// console.log(a);
+
+//splice
+// var a=["car1","car2","car3"]
+// a.splice(2,1);
+
+// console.log(a);
+
+
+
+// var cleancities=["karachi","islamabad","lahore","pindi"];
+// var sum=[];
+// for(var i=0;i<cleancities.length;i++){
+//     sum.push(cleancities[i]);
+//     console.log(sum);
+    
+// }
+
+// var match=false;
+
+// var cleancities=["karachi","islamabad","lahore","kashmir","baluchistan"];
+// var check=prompt("Enter the city of Pakistan")
+// for(var i=0; i<cleancities.length;i++){
+
+//     if(check===cleancities[i]){
+//         match=true;
+//         console.log("It is cleanist city");
+       
+        
+//     }
+// }
+
+// if(match===false){
+//     console.log("False city");
+    
+// }
+
+//NaN
+// var a=false;
+
+// console.log(a);
 
 
 
