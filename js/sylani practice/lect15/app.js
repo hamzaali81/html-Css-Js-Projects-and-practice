01592d4dab51bfbea1d6e70f268278ca2738e676
hamzaali <hamza54834@gmail.com>
2020-06-23 04:18:33 -0700

    js complete

---
+++ js/sylani practice/lect15/app.js
@@ -0,0 +1,85 @@
+ //Varible convert string
+//  var a=25;
+ 
+//  var b= "53";
+// console.log(a+parseFloat(b));
+// // var a=10;
+// // var a=a+4;
+// // console.log(a);
+
+// var a=10;
+// a+=4;
+// console.log(a);
+
+// var a=["hamza","ahmed","ali"];
+// // for(var i=0; i<a.length;i++){
+// //     console.log(a[i]);
+    
+// // }
+
+
+// a.forEach(abc => {console.log(abc);});
+
+// a.map(el=>{el.a;
+//     console.log(el);
+//     })
+
+
+// for (a=1;a<=10;a++){
+
+//     console.log(a);
+   
+// }
+// var a=1;
+// console.log(a++);
+// console.log(a)
+// var a=10;
+// var c= a++ + --a + --a + ++a + a++ + a;
+//     //10      10     9   10    10    11
+// console.log(c);
+
+
+// BDMAS rule
+
+// var a=10 + 2 * 3 / 2;
+// console.log(a);
+// var a=prompt("Your age is",a);
+// console.log(a);
+
+var age=18;
+var name="hamza"
+if(age>=18 && name!=="hamza"){
+    console.log("you are egligible");
+    
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
