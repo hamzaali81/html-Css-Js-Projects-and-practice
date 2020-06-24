f2f6b92ba4d5739f4a8101a91fe62ef40f9ef670
hamzaali <hamza54834@gmail.com>
2020-06-24 11:54:27 -0700

    js solve 1- 20

---
+++ js/sylani practice/17-20/app.js
@@ -0,0 +1,242 @@
+// var a=[
+//     ["hamza"," designer ","karachi"],
+//     ["ali"," developer ","lahore"]
+// ] 
+// var a=[
+//     [ ],
+//     [ ]
+// ]
+//  a[0][0]="hamza";
+//  a[0][1]="designer";
+//  a[0][2]="karachi";
+//  a[1][0]="ali";
+//  a[1][1]="developer";
+//  a[1][2]="lahore";
+ 
+
+// for(i=0;i<3;i++){
+//     for(j=0;j<3;j++){
+//         document.write(a[i][j])
+//     }
+//     document.write("<br>")
+// }
+
+
+
+// var a=[
+//     [0,1,2,3],
+//     [1,0,1,2],
+//     [2,1,0,1]
+// ]
+// // var b=[1,2];
+// // var b=[[[1,2],[2,3]]]
+// // for(var i=0;i<a.length;i++){
+// //     for(var j=0;j<a.length;j++){
+// //       console.log(a[i][j]);
+    
+        
+// //     }
+// //     // console.log(a[i][j]);
+// //     break;
+// // }
+// for(var i=0;i<4;i++){
+//     for(var j=0;j<4;j++){
+//     //   console.log(a[i][j]);
+// document.write(a[i][j])
+//     // break
+        
+//     }
+//     // console.log("<br>");
+
+//     document.write("<br>")
+//     // console.log(a[i][j]);
+//     // break;
+// }
+
+
+
+// var a=10;
+// for (a=1;a<=10;a++){
+//     document.write(a+"<br>")
+// }
+// var table=prompt("Which table");
+// var tablelength=prompt("what length of table")
+// for(var i=1;i<=tablelength;i++){
+//     console.log(table+ "*"+i+" = " +i*table);
+    
+// }
+
+
+// var fruits=["apple","banana","mango","orange","straberry"];
+// for(var i=0;i<fruits.length;i++){
+//     // document.write(fruits[i]+"<br>")
+//     document.write("Elements of Array index "+fruits[i] +fruits.indexOf(fruits[i]) +"<br>")
+// }
+
+
+
+// var a = ["cake", "applpie", "cooke", "chip", "patt"];
+// var search=prompt("Serch fruit")
+// for(var i=0;i<a.length;i++){
+//     if(search===a[i]){
+//         document.write("Yes available in this list "+a[i]);
+//     }
+// }
+
+// var a=[1,2,3];
+// var b=[0];
+// var c=[]
+
+// for(var i=0;i<a.length;i++){
+//     for(var j=0;j<b.length;j++){
+//     c.push(a[i] + b[j]);
+// document.write(c);     
+//     }
+//     document.write("<br>");
+// }
+
+
+
+
+// var firstNames = ["BlueRay ", "Upchuck ", "Lojack ", "Gizmo ", "Do-Rag "];
+//  var lastNames = ["Zzz", "Burp", "Dogbone", "Droop"];
+//  var fullNames = [];
+//  for (var i = 0; i < firstNames.length; i++) {
+//  for (var j = 0; j < lastNames.length; j++) {
+// fullNames.push(firstNames[i] + lastNames[j]);
+// document.write(fullNames,"<br>");
+
+
+//  }
+//  }
+
+// for(var a=10; a>=1;a--){
+//     console.log(a);
+    
+// }
+
+// for(var a=0; a<=20;a++){
+//    if(a%2!==0){
+//        console.log("Odd number "+a);
+       
+//    }
+    
+// }
+// for(var a=0; a<=20;a++){
+//     if(a%2===0){
+//         console.log("Odd number "+a);
+        
+//     }
+     
+//  }
+
+// for(var a=1; a<=10;a++){
+//     console.log(a*2+"K");
+    
+// }
+
+
+//  (0 * 1)/2
+
+// var a=[24,53,78,91,12];
+// var b=Math.max.apply(null,a)
+// console.log(b);
+
+
+//  var a=[24,53,78,91,12];
+
+// for(var i=0;i<a.length;i++){
+//   if(a[i]<=100){
+// console.log("largest value"+a[i]);
+
+//   }
+    
+    
+    
+// }
+// const array = [-1,0,3,100, 99, 2, 99]
+
+
+// function biggestNumberInArray(arr) {
+//     for (let i = 0; i < array.length; i++) {
+//        for(let j=1;j<array.length;j++){
+//            for(let k =2;k<array.length;k++){
+//                if(array[i]>array[j] && array[i]>array[k]){
+//                      console.log(array[i]);
+//             }
+//           }
+//        }
+//     }
+//  }
+// biggestNumberInArray(array[i][j[k]])
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
