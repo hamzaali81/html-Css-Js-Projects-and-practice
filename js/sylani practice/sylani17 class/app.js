a4d00191bce1ae3a90310bd82020de0a7e90f201
hamzaali <hamza54834@gmail.com>
2020-07-01 19:48:29 -0700

    DOM

---
+++ js/sylani practice/sylani17 class/app.js
@@ -0,0 +1,112 @@
+// var text="Lorem ipsum dolor sit amet.<ul><li>Home</li><li>Home</li><li>Home</li><li>Home</li></ul>";
+
+
+// function test(){
+//     var a=document.getElementById('pera').innerHTML=text;
+
+// }
+
+
+// function ChangeCar(){
+//     var car=document.getElementById('cars');
+//     car.src='car2.jpg';
+// }
+
+// function changeCar2(){
+//     var car =document.getElementById('cars');
+//     car.src='car1.jpg';
+// }
+
+
+// function ChangeCar(id,src){
+//     var car=document.getElementById(id);
+//     car.src=src;
+// }
+
+// function changeCar2(){
+//     var car =document.getElementById('cars');
+//     car.src='car1.jpg';
+// }
+
+// var parent=document.getElementById('parent');
+// var pera=parent.getElementsByTagName('p');
+
+// for(var i=0;i<pera.length;i++){
+//     pera[i].style.color='red';
+// }
+
+// 'use strict';
+
+// var a=20;
+//  ()=>{
+//     'use strict';
+
+//     var a=100;
+//     var b=200;
+// }
+
+
+// console.log(a);
+
+
+// debugger;
+var p = document.getElementById('cont');
+// var contents=p.childNodes[0];
+// var contents=p.lastChild;
+// var contents=p.nextSibling;
+// var contents=p.previousSibling;
+// var contents=p.parentNode;
+
+
+var t=p.firstChild;
+console.log(t);
+var a=t.nodeValue;
+// document.write(a)
+
+// console.log(contents);
+
+// var p = document.getElementById('cont');
+// var counter=0
+
+// for(var i=0;i<p.childNodes.length;i++){
+//     console.log(p[i]);
+//     if(p.childNodes===1){
+// counter+=1
+//     }
+//     if(p.childNodes===2){
+//         p.childNodes[i].innerHTML="All his men"
+//     }
+    
+// }
+// var d = document.getElementById("cont");
+//  var a = d.getElementsByTagName("p");
+//  var contents = a[0].innerHTML;
+// console.log(contents);
+// console.log(a);
+
+
+
+// var a=['hamza','ali','ahmed'];
+// for (b of a){
+//     document.write(b+"<br>");
+    
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
