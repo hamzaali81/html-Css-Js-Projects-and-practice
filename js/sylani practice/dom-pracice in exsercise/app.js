d2a24842284f22c1ae1cef415c7de3b9068b5d0d
hamzaali <hamza54834@gmail.com>
2020-07-03 19:29:06 -0700

    form changes

--- js/sylani practice/dom-pracice in exsercise/app.js
+++ js/sylani practice/dom-pracice in exsercise/app.js
@@ -24,47 +24,179 @@ function showSuccess(input){
 //Check email is valid
 
 function isValidEmail(email){
-const re= ;
+    const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
 return re.test(String(email).toLowerCase())
 }
+//checked required field
 
+function checkRequired(inputArr){
+  inputArr.forEach(function(input){
+// console.log(input.value);
 
+if(input.value.trim()===''){
+    showError(input,`${input.id} is required`)
+}
+else{
+    showSuccess(input);
+}
+
+  })
+}
+//Get fieldname
+function getFieldName(input){
+return input.id.charAt(0).toUpperCase() + input.id.slice(1)
+}
 
 //Event listener
 form.addEventListener('submit',function(e){
     e.preventDefault();
     // console.log(username.value);
-    
-   if(username.value===''){
-// alert('username is required')
-// username.style.border='2px solid red'
-showError(username,'Username is required')
-}else{
-    showSuccess(username);
-}
-if(email.value===''){
-    // alert('username is required')
-    // username.style.border='2px solid red'
-    showError(email,'Email is required')
-    }else{
-        showSuccess(email);
-    }
-    if(password.value===''){
-        // alert('username is required')
-        // username.style.border='2px solid red'
-        showError(password,'Password is required')
-        }
-        else if(!isValidEmail(email.value)){
-            showError(email,'Email is not valid')
-        }
-        else{
-            showSuccess(password);
-        }
-        if(password2.value===''){
-            // alert('username is required')
-            // username.style.border='2px solid red'
-            showError(password2,'Password 2 is required')
-            }else{
-                showSuccess(password2);
-            }
+    // checkRequired(username);
+    //checkRequired(password)
+checkRequired([username,email,password,password2])   
 })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const form =document.getElementById('form');
+// const username =document.getElementById('username');
+// const email =document.getElementById('email');
+// const password =document.getElementById('password');
+// const password2 =document.getElementById('password2');
+
+
+
+// //show input error message
+// function showError(input,message){
+// const formControl=input.parentElement;
+// formControl.className = 'form-control error';
+
+// const small = formControl.querySelector('small');
+// small.innerText=message;
+// }
+
+// //show Success  outline
+// function showSuccess(input){
+//     const formControl=input.parentElement;
+//     formControl.className='form-control success';
+// }
+
+// //Check email is valid
+
+// function isValidEmail(email){
+// const re= ;
+// return re.test(String(email).toLowerCase())
+// }
+
+
+
+// //Event listener
+// form.addEventListener('submit',function(e){
+//     e.preventDefault();
+//     // console.log(username.value);
+    
+//    if(username.value===''){
+// // alert('username is required')
+// // username.style.border='2px solid red'
+// showError(username,'Username is required')
+// }else{
+//     showSuccess(username);
+// }
+// if(email.value===''){
+//     // alert('username is required')
+//     // username.style.border='2px solid red'
+//     showError(email,'Email is required')
+//     }else{
+//         showSuccess(email);
+//     }
+//     if(password.value===''){
+//         // alert('username is required')
+//         // username.style.border='2px solid red'
+//         showError(password,'Password is required')
+//         }
+//         else if(!isValidEmail(email.value)){
+//             showError(email,'Email is not valid')
+//         }
+//         else{
+//             showSuccess(password);
+//         }
+//         if(password2.value===''){
+//             // alert('username is required')
+//             // username.style.border='2px solid red'
+//             showError(password2,'Password 2 is required')
+//             }else{
+//                 showSuccess(password2);
+//             }
+// })
