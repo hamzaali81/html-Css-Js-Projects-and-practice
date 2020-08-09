5b8c18b0d767d72fdeadf975d2df6426b0aeb834
hamzaali <hamza54834@gmail.com>
2020-08-09 10:41:27 -0700

    new concept js

---
+++ js classes/class 01/app.js
@@ -0,0 +1,19 @@
+
+//1.pre & post increment 
+// var num1= 5;
+// var num2=9;
+
+// console.log(num1++ + ++num1 + --num2 + --num2);
+
+// //2.prompt
+// var result=prompt("What is your name","Enter Yor Name");
+// console.log("Welcome to my company "+result.toUpperCase());
+
+
+//case 01
+//convert string
+var num1 = 3;
+var num2 = prompt("Enter Number to add + 3")
+
+console.log(num1 + parseInt(num2));
+
