3f5f27d7ecc17bfa5c36ddc597f799c6bf90256e
hamzaali <hamza54834@gmail.com>
2020-08-22 12:18:05 -0700

    page popup

--- js classes/reload Animation/app.js
+++ js classes/reload Animation/app.js
@@ -1,12 +1,21 @@
-var screen=document.getElementById('load-screen');
-var loading=document.getElementById('loading');
-// setTimeout({function(){
-    // screen.
-// }
+// var screen=document.getElementById('load-screen');
+// var loading=document.getElementById('loading');
+// // setTimeout({function(){
+//     // screen.
+// // }
 
-// },1000)
+// // },1000)
 
-window.addEventListener('load',function(){
-    screen.style.fadeOut='slow';
-    screen.style.display= 'none';
-})
+// window.addEventListener('load',function(){
+//     screen.style.fadeOut='slow';
+//     screen.style.display= 'none';
+// })
+
+//popup
+document.getElementById('button').addEventListener("click", function() {
+	document.querySelector('.bg-modal').style.display = "flex";
+});
+
+document.querySelector('.close').addEventListener("click", function() {
+	document.querySelector('.bg-modal').style.display = "none";
+});
