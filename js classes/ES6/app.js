e0c480bebf1da2b234e0add678e2ed90e7469de3
hamza ali <hamza54834@gmail.com>
2020-09-03 08:08:44 -0700

    ES6 start

---
+++ js classes/ES6/app.js
@@ -0,0 +1,45 @@
+// ES6 
+// var 
+// let
+// const
+
+
+//1.var
+// var name='hamza';
+// name ='ali'
+
+// console.log(name);
+// let name='hamza';
+// name ='ali';
+// console.log(name);
+
+// var name='hamza';
+// function checkUser(){    
+//     var username=prompt("Enter your name");
+//     // name=username
+//      name=username //override
+//     if(name==='ali'){
+//         name='baber azam';
+//         console.log(name);
+//     }
+//     console.log(name);
+
+// }
+// console.log(name);
+// checkUser();
+// console.log(name);
+
+
+
+//2.Arrow function
+// function abc(){
+//     return var v=10;
+// }
+
+
+var abc=(v)=> 
+ v='hello';
+// console.log(v);
+
+var test=abc();
+console.log(test);
