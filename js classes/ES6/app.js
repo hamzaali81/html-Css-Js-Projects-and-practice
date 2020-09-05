f183c5e7d82ade7f5f08bcf9f7155fa49e8707f1
hamza ali <hamza54834@gmail.com>
2020-09-05 13:30:56 -0700

    IIFE

--- js classes/ES6/app.js
+++ js classes/ES6/app.js
@@ -44,21 +44,39 @@
 // var test=abc();
 // console.log(test);
 
+//Advance JS
+//IIFE
+var score2=3;
+(function(){
+    var score=4;
+    console.log(score);
+    setTimeout(()=>{
+        console.log(score3);
+
+    },4000)
+})()
+        function test(){
+                    var score1=4;
+                    console.log(score1,score2);
+                }
+                test()
+console.log(score2); 
+
 
 
 //ES6 Advance
 // forEach
 
 // ES5
-var math=Math.ceil(Math.random()*3);
-// console.log(math);
-var colors=['red','blue','green'];
-for(var i=0;i<colors.length;i++){
-    console.log(colors[i],i);
-}
+// var math=Math.ceil(Math.random()*3);
+// // console.log(math);
+// var colors=['red','blue','green'];
+// for(var i=0;i<colors.length;i++){
+//     console.log(colors[i],i);
+// }
 
 
-colors.forEach((el,index)=>{console.log(el,index);})
+// colors.forEach((el,index)=>{console.log(el,index);})
 
 
 
