22da76ddc0bdb3d23313375416437f0b4bc273c5
hamza ali <hamza54834@gmail.com>
2020-09-05 12:18:12 -0700

    advance js move

--- js classes/ES6/app.js
+++ js classes/ES6/app.js
@@ -50,15 +50,15 @@
 // forEach
 
 // ES5
-var math=Math.ceil(Math.random()*355689);
-console.log(math);
+var math=Math.ceil(Math.random()*3);
+// console.log(math);
 var colors=['red','blue','green'];
 for(var i=0;i<colors.length;i++){
-    console.log(colors[i]);
+    console.log(colors[i],i);
 }
 
 
-colors.forEach((el,index)=>{console.log(el,`${index}${math}`);})
+colors.forEach((el,index)=>{console.log(el,index);})
 
 
 
